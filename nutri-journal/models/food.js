842b5351c1067920dfd7c858a95f8cc0c90bcc3e
Timothy Soon <timothysoonyufeng@gmail.com>
2023-07-24 03:57:28 +0800

    added back food model

--- nutri-journal/models/food.js
+++ nutri-journal/models/food.js
@@ -3,6 +3,22 @@ const Schema = mongoose.Schema;
 
 const foodSchema = new Schema({
     name: { type: String, required: true, trim: true },
+    carbohydrate: { type: Number, required: true, min: 0 },
+    protein: { type: Number, required: true, min: 0 },
+    fat: { type: Number, required: true, min: 0 },
+    trans_Fat: { type: Number, required: true, min: 0 },
+    saturated_Fat: { type: Number, required: true, min: 0 },
+    polyunsaturated_Fat: { type: Number, required: true, min: 0 },
+    monounsaturated_Fat: { type: Number, required: true, min: 0 },
+    cholesterol: { type: Number, required: true, min: 0 },
+    sodium: { type: Number, required: true, min: 0 },
+    potassium: { type: Number, required: true, min: 0 },
+    fiber: { type: Number, required: true, min: 0 },
+    sugar: { type: Number, required: true, min: 0 },
+    vitamin_A: { type: Number, required: true, min: 0 },
+    vitamin_C: { type: Number, required: true, min: 0 },
+    calcium: { type: Number, required: true, min: 0 },
+    iron: { type: Number, required: true, min: 0 },
 }, {
     timestamps: true,
 });
